9bd88df6c50a5740c5ec148696aee7b261fe31e9
Atanas Chuchulev <atanas.v4@gmail.com>
2021-05-12 11:50:49 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,27 @@
+# data "external" "powershell_execution" {
+#   program = [
+#     "ps-linux-x64/pwsh",
+#     "./date.ps1"]
+# }
 
-data "external" "powershell_execution" {
-  program = [
-    "ps-linux-x64/pwsh",
-    "./date.ps1"]
+resource "null_resource" "ps1" {
+  triggers = {
+    uuid = uuid() # re-creates null_resource on every run
+  }
+  provisioner "local-exec" {
+    command = <<EOH
+
+# download the powershell '.tar.gz' archive
+curl -OL https://github.com/PowerShell/PowerShell/releases/download/v7.1.3/powershell-7.1.3-linux-x64.tar.gz
+
+# extract powershell to the target folder
+tar -zxf powershell-7.1.3-linux-x64.tar.gz
+
+# Set execute permissions
+chmod +x pwsh
+
+# Do some command with ./pwsh
+./pwsh -Command Get-Date -Uformat "%Y-%m-%d"
+EOH
+  }
 }
